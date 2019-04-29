352d12cbe8665258400f756d125be1a87323dccc
Robert Cooper <robert.cooper.qu@gmail.com>
2019-04-29 16:53:25 -0400

    chore: lint & format all files in the repo (#14)
    * feat(linting-and-formatting): setup repo with linting and formatting
    
    * docs(eslint-config-landr): update README
    
    * chore(eslint-config-landr): format files
    
    * chore(eslint-config-landr): bump package number
    
    * docs(prettier-config-landr): add note on overriding settings
    
    * chore(prettier-config-landr): remove package-lock.json
    
    * chore(prettier-config-landr): format files
    
    * chore(prettier-config-landr): bump version number
    
    * chore(stylelint-config-landr): lint files
    
    * chore(stylelint-config-landr): bump version number

--- prettier-config-landr/index.js
+++ prettier-config-landr/index.js
@@ -1,26 +1,26 @@
 module.exports = {
-    "singleQuote": true,
-    "semi": true,
-    "printWidth": 100,
-    "trailingComma": "all",
-    "overrides": [
+    singleQuote: true,
+    semi: true,
+    printWidth: 100,
+    trailingComma: 'all',
+    overrides: [
         {
-            "files": "*.tpl.html",
-            "options": {
-                "parser": "angular"
-            }
+            files: '*.tpl.html',
+            options: {
+                parser: 'angular',
+            },
         },
         {
-            "files": "*.js",
-            "options": {
-                "trailingComma": "es5"
-            }
+            files: '*.js',
+            options: {
+                trailingComma: 'es5',
+            },
         },
         {
-            "files": "*.{js,ts,jsx,tsx,scss,json,html}",
-            "options": {
-                "tabWidth": 4,
-            }
+            files: '*.{js,ts,jsx,tsx,scss,json,html}',
+            options: {
+                tabWidth: 4,
+            },
         },
-    ]
-}
+    ],
+};

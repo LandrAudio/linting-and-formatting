f0cab5b7db27f8ef378798b4583e143fdd97f8fc
Maxime Péloquin <maxime.peloquin@gmail.com>
2024-01-03 09:45:00 -0500

    fix(lingui): move lingui to dependecies (#114)

--- packages/eslint-config-landr/lingui.js
+++ packages/eslint-config-landr/lingui.js
@@ -2,18 +2,6 @@ module.exports = {
     extends: [],
     plugins: ['lingui'],
     rules: {
-        'no-restricted-imports': [
-            'error',
-            {
-                paths: [
-                    {
-                        name: '@lingui/react',
-                        importNames: ['Trans'],
-                        message: 'Please import Trans from @lingui/macro instead.',
-                    },
-                ],
-            },
-        ],
         'lingui/t-call-in-function': 'error',
         'lingui/no-expression-in-message': 'warn',
         'lingui/no-single-tag-to-translate': 'warn',

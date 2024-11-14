b840825fba3e93fd97ef1a3eff35297a749435af
Lukas Switlicki <lukas.switlicki@gmail.com>
2024-11-14 11:34:40 -0500

    feat: add react google translate plugin

---
+++ packages/eslint-config-landr/translate.js
@@ -0,0 +1,8 @@
+module.exports = {
+    extends: [],
+    plugins: ['react-google-translate'],
+    rules: {
+        'react-google-translate/no-conditional-text-nodes-with-siblings': 'warn',
+        'react-google-translate/no-return-text-nodes': 'warn',
+    },
+};
